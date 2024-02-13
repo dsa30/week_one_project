d77622fb24a976c9181196df6aabc7f5591f4ee1
Daniel Selase Anyidoho <adanielselase@gmail.com>
2024-02-13 12:13:57 +0000

    complete week one project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import './App.css';
-import Card from './components/Card';
-import sedans from "./images/icon-sedans.svg"
-import suvs from "./images/icon-suvs.svg"
-import luxury from "./images/icon-luxury.svg"
+import "./App.css";
+import Card from "./components/Card";
+import sedans from "./images/icon-sedans.svg";
+import suvs from "./images/icon-suvs.svg";
+import luxury from "./images/icon-luxury.svg";
 
 function App() {
   return (
     <div className="container">
-     <Card heading="SEDANS" image={sedans} background="hsl(31, 77%, 52%)" btcolor="hsl(31, 77%, 52%)" paragraph="Choose a sedan for its affordability and excelent fuel economy. Ideal for cruising in the city of on your next road trip." />
-     <Card heading="SUVS" image={suvs} background="hsl(184, 100%, 22%)" btcolor="hsl(184, 100%, 22%)" paragraph="Take and SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures." />
-     <Card heading="LUXURY" image={luxury} background="hsl(179, 100%, 13%)" btcolor="hsl(179, 100%, 13%)"paragraph="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of luxury rental and arrive in style." />
+      <Card
+        heading="SEDANS"
+        image={sedans}
+        background="hsl(31, 77%, 52%)"
+        btcolor="hsl(31, 77%, 52%)"
+        paragraph="Choose a sedan for its affordability and excelent fuel economy. Ideal for cruising in the city of on your next road trip."
+      />
+      <Card
+        heading="SUVS"
+        image={suvs}
+        background="hsl(184, 100%, 22%)"
+        btcolor="hsl(184, 100%, 22%)"
+        paragraph="Take and SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+      />
+      <Card
+        heading="LUXURY"
+        image={luxury}
+        background="hsl(179, 100%, 13%)"
+        btcolor="hsl(179, 100%, 13%)"
+        paragraph="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of luxury rental and arrive in style."
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Card = ({heading, paragraph, image, background, btcolor}) => {
+const Card = ({ heading, paragraph, image, background, btcolor }) => {
+  const [color, setColor] = useState({
+    color: btcolor,
+    border: "2px solid white",
+  });
   return (
-      <div style={{padding: "20px", backgroundColor: `${background}`}}>
-        <img src={image} alt={image} />
-        <h1>{heading}</h1>
-        <p>{paragraph}</p>
-        <button style={{color: `${btcolor}`}}>Learn More</button>
-      </div>
-  )
-}
+    <div className="card" style={{ backgroundColor: `${background}` }}>
+      <img src={image} alt={image} />
+      <h1 className="heading">{heading}</h1>
+      <p className="para">{paragraph}</p>
+      <button
+        style={color}
+        onMouseEnter={() => {
+          setColor({
+            color: "white",
+            backgroundColor: "transparent",
+            border: "2px solid white",
+          });
+        }}
+        onMouseLeave={() => {
+          setColor({ color: btcolor, border: "2px solid white" });
+        }}
+      >
+        Learn More
+      </button>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
